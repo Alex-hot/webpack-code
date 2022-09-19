7f95d3232f54023d0437ec765fbea58be72c6c07
luotao <15256449571@163.com>
2022-09-19 11:13:43 +0800

    处理stylus资源

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './assets/styles/index.css';
 import './assets/styles/index.less';
 import './assets/styles/index.sass';
 import './assets/styles/index.scss';
+import './assets/styles/index.styl';
 
 console.log(conut(2, 1));
 console.log(sum(1, 2, 3, 4));
